ce73499bc65e93f2e84d0f5a31d705f2e30ac1da
Dan Zajdband <dan.zajdband@gmail.com>
2012-06-21 10:56:46 -0300

    publish 0.0.1

--- example.js
+++ example.js
@@ -1,7 +1,8 @@
 var mdb = require('./lib/moviedb');
 
-var m = new mdb('yout api key');
+var m = new mdb('dc4940972c268b026150cf7be6f01d11');
 
 m.searchMovie({query: 'alien'}, function(err, res){
+  console.log(err);
  console.log(res);
 });
