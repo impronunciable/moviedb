1288299d25cf635e74af866d7e89f1d1c98d8e8b
Gunay Mert Karadogan <mertkaradogan@gmail.com>
2013-12-04 13:25:55 +0100

    Add a wrapper around moviedb.js
    Add findMovieByImdb to get all info by using an imdb_id.
    Update example.

--- example.js
+++ example.js
@@ -1,10 +1,5 @@
-var mdb = require('./lib/moviedb')('dc4940972c268b026150cf7be6f01d11');
+var mdb = require('./lib/wrapper');
 
-mdb.movieInfo({id: 11}, function(err, res){
-  console.log('--------','popular');
-  console.log(res);
-}).miscPopularMovies({}, function(err, res){
-  console.log(err);
-  console.log('--------','ninja');
-  console.log(res);
+mdb.findMovieByImdb("tt1727388", function(err, res){
+	console.log(res);
 });
