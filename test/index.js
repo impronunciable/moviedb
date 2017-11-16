fa900032655e2fbcc243d7ecbc53cb0eaeb0ac3c
Grant Holle <hollegrant@gmail.com>
2017-11-16 09:38:15 +0800

    Clean up the test file

--- test/index.js
+++ test/index.js
@@ -1,73 +1,65 @@
-var should = require('chai').should()
-var assert = require('chai').assert
-var colors = require('colors')
-var apiKey = process.env.MOVIEDB_API_KEY || process.env.npm_config_key
-var api
+'use strict'
+
+/* global describe it */
+const assert = require('chai').assert
+const apiKey = process.env.MOVIEDB_API_KEY || process.env.npm_config_key
+const MovieDb = require('../index.js')
+require('chai').should()
+require('colors')
 
 /**
- * checks for missing API key
+ * Checks for missing API key.
  *
- * the proper way to run the test
- *
- * 		npm test --key='{your api key}'
- *
- * @param  {[type]} !apiKey ||            apiKey.length [description]
- * @return {[type]}         [description]
+ * The proper way to run the test:
+ * npm test --key='{your api key}'
  */
 if (!apiKey || apiKey.length === 0) {
   console.log('You have not provided the API key'.red)
-  console.log('	Running tests:'.cyan)
-  console.log('	npm test --key="{your api key}"'.cyan)
+  console.log('\tRunning tests:'.cyan)
+  console.log('\tnpm test --key="{your api key}"'.cyan)
   throw new Error('Missing API key, please `run npm test --key="{your api key}"`')
 }
 
-api = require('../index.js')(apiKey)
+const api = new MovieDb(apiKey)
 
 describe('moviedb', function () {
   this.timeout(10000)
 
-	// basic movie search
-  it('should search for Zoolander', function (done) {
-    api.searchMovie({query: 'Zoolander' }, function (err, res) {
-      if (err) done(err)
-			// console.log(res);
+  // basic movie search
+  it('should search for Zoolander', done => {
+    api.searchMovie({ query: 'Zoolander' }).then(res => {
       res.should.be.an('object')
       res.should.have.property('results')
       res.results.should.be.an('array')
       done()
-    })
+    }).catch(done)
   })
 
-  it('should get the tv shows airing today', function (done) {
-    api.tvAiringToday(function (err, res) {
-      if (err) done(err)
-			// console.log(data);
+  it('should get the tv shows airing today', done => {
+    api.tvAiringToday().then(res => {
       res.should.be.an('object')
       res.should.have.property('results')
       res.results.should.be.an('array')
       done()
-    })
+    }).catch(done)
   })
 
-  it('should get the tv shows OnTheAir', function (done) {
-    api.tvOnTheAir(function (err, res) {
-      if (err) done(err)
-			// console.log(data);
+  it('should get the tv shows OnTheAir', done => {
+    api.tvOnTheAir().then(res => {
       res.should.be.an('object')
       res.should.have.property('results')
       res.results.should.be.an('array')
       done()
-    })
+    }).catch(done)
   })
 
-  it('should get the movie release dates', function (done) {
-    api.movieReleaseDates({id: 209112}, function (err, res) {
-      if (err) done(err)
+  it('should get the movie release dates', done => {
+    api.movieReleaseDates({ id: 209112 }).then(res => {
       res.should.be.an('object')
       res.should.have.property('results')
       res.results.should.be.an('array')
       assert.equal(res.id, 209112)
       done()
-    })
+    }).catch(done)
   })
 })
