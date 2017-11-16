4d7e0bea357008876fd0709d5111a4bfd8e0f310
Grant Holle <hollegrant@gmail.com>
2017-11-16 09:37:53 +0800

    Convert the module to a class, implement promises, implement limit, and moved to the index file
    Sorry this couldn't be more atomic. This is a big one... The module is now an es6 class. Instead of using callbacks, promises are implemented, which will fix #64. Limit.js is also used which was talked about in PR #74. Instead of using the index file to just point to the class in the lib, move the class into the index file. It seemed a little redundant to me.

--- index.js
+++ index.js
@@ -1,2 +1,117 @@
+'use strict'
 
-module.exports = require('./lib/moviedb')
+const request = require('superagent')
+const endpoints = require('./lib/endpoints')
+const limits = require('limits.js')
+const throttle = limits().within(10 * 1000, 39)
+
+module.exports = class {
+  constructor(apiKey, baseUrl = 'https://api.themoviedb.org/3/') {
+    if (!apiKey) {
+      throw new Error('Bad api key')
+    }
+
+    this.apiKey = apiKey
+    this.baseUrl = baseUrl
+
+    // Create the dynamic api methods using the configuration found in lib/endpoints
+    Object.keys(endpoints.methods).forEach(method => {
+      const met = endpoints.methods[method]
+
+      Object.keys(met).forEach(m => {
+        this[method + m] = (params = {}) => {
+          if (!this.token || Date.now() > +new Date(this.token.expires_at)) {
+            return this.requestToken()
+              .then(() => this.makeRequest(met[m].method, params, met[m].resource))
+          }
+
+          return this.makeRequest(met[m].method, params, met[m].resource)
+        }
+      })
+    })
+  }
+
+  /**
+   * Gets an api token using an api key
+   *
+   * @returns {Promise}
+   */
+  requestToken() {
+    return new Promise((resolve, reject) => {
+      request
+        .get(this.baseUrl + endpoints.authentication.requestToken)
+        .query({ 'api_key': this.apiKey })
+        .end((err, res) => {
+          if (err) {
+            return reject(err)
+          }
+
+          this.token = res.body
+          resolve()
+        })
+    })
+  }
+
+  /**
+   * Gets the session token
+   *
+   * @returns {Promise}
+   */
+  session() {
+    return new Promise((resolve, reject) => {
+      request
+        .get(this.baseUrl + endpoints.authentication.session)
+        .query({ 'api_key': this.apiKey, 'request_token': this.token.request_token })
+        .end((err, res) => {
+          if (err || !res.body.success) {
+            return reject(err || res.body)
+          }
+
+          this.sessionId = res.body.session_id
+          resolve()
+        })
+    })
+  }
+
+  /**
+   * Makes the request to the api using the configuration from lib/endpoints
+   *
+   * @param {String} type The http verb
+   * @param {Object} params The parameters to pass to the api
+   * @param {String} endpoint The api endpoint relative to the base url
+   * @returns {Promise}
+   */
+  makeRequest(type, params, endpoint) {
+    return new Promise((resolve, reject) => {
+      endpoint = endpoint.replace(':id', params.id).replace(':season_number', params.season_number).replace(':episode_number', params.episode_number)
+      type = type.toUpperCase()
+
+      let req = request(type, this.baseUrl + endpoint)
+        .query({ api_key: this.apiKey, session_id: this.sessionId })
+
+      if (params.ifNoneMatch) {
+        req = req.set('If-None-Match', params.ifNoneMatch)
+      } else if (params.ifModifiedSince) {
+        let t = params.ifModifiedSince
+
+        if (t.toUTCString) {
+          t = t.toUTCString()
+        }
+
+        req = req.set('If-Modified-Since', t)
+      }
+
+      req[type === 'GET' ? 'query' : 'send'](params)
+
+      throttle.push(() => {
+        req.end((err, res) => {
+          if (err) {
+            return reject(err)
+          }
+
+          resolve(res.body, res)
+        })
+      })
+    })
+  }
+}
